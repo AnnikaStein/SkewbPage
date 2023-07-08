472043e1c28aa5ec4d7f4c221db4df1f01ae854e
Annika Stein <annika-stein@t-online.de>
2023-07-08 14:43:23 +0200

    feat: random orientation for OLT with pre-/post-rotations
    Co-Authored-By: Malte Ihlefeld <37628763+mihlefeld@users.noreply.github.com>

--- SkewbSkills/skewbskillsscripts.js
+++ SkewbSkills/skewbskillsscripts.js
@@ -1361,7 +1361,7 @@ function ScramblePlusColour() {
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
-  
+
 
 function ScramblePlusColourAlg() {
     if (scramblelistAlg.length == 0) {
@@ -1408,6 +1408,27 @@ function ScramblePlusColourOL() {
 
   var scramblezumanzeigenOL = scramblelistOL[scramblelistOL.length - 1];
   scramblelistOL.pop();
+  // get random int for top site color: 0, 1, 2, 3: /, x, x2, x' | 4, 5: z, z'
+  // get random int for side color: 0, 1, 2, 3: /, y, y2, y'
+  // if x2y2 do z2 instead
+  var firstRotations = ["", "x ", "x2 ", "x' ", "z ", "z' ", "z2 "]
+  var firstRotationsInv = ["", " x'", " x2", " x", " z'", " z", " z2"]
+  var secondRotations = ["", "y ", "y2 ", "y' "]
+  var secondRotationsInv = ["", " y'", " y2", " y"]
+  var firstRotationIndex = getRandomInt(6);
+  var secondRotationIndex = getRandomInt(4);
+  if (firstRotationIndex == 2 && secondRotationIndex == 2) {
+      firstRotationIndex = 6;
+      secondRotationIndex = 0;
+  }
+  var firstRotation = firstRotations[firstRotationIndex];
+  var secondRotation = secondRotations[secondRotationIndex];
+  var firstRotationInv = firstRotationsInv[firstRotationIndex];
+  var secondRotationInv =  secondRotationsInv[secondRotationIndex];
+  var rotations = firstRotation + secondRotation;
+  var backRotations = secondRotationInv + firstRotationInv;
+  scramblezumanzeigenOL = rotations + scramblezumanzeigenOL + backRotations;
+
   document.getElementById("scramblelabelOL").innerHTML = doubleMoveRemover(scramblezumanzeigenOL + " ");
 
   if (showscrimageOLT.checked == true) {
